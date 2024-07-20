89f858a7d5d9b591a2ce91e21114817fc384580d
Eskedar Tesfaye <eskedartesfaye31@gmail.com>
2024-07-20 22:25:05 +0300

    all

--- quiz.js
+++ quiz.js
@@ -1,27 +1,17 @@
 function checkAnswer() {
     
     let correctAnswer = "4";
-    
-    
     let userAnswerElement = document.querySelector('input[name="quiz"]:checked');
-    
+    let userAnswer = userAnswerElement.value;
     
     let feed = document.querySelector('#feedback');
     
-    
-    
-        let userAnswer = userAnswerElement.value;
-        
-        
         if (correctAnswer === userAnswer) {
             feed.textContent = "Correct! Well done.";
         } else {
             feed.textContent = "That's incorrect. Try again!";
         }
-}
-
-
-
+    }
 let answerButton = document.getElementById('submit-answer');
 answerButton.addEventListener('click', function(){
     checkAnswer();
